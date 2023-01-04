967e353c2f2d91c6d2b435fb67ccdbfc3c5be8b4
adityagour <adityagour2812@gmail.com>
2023-01-05 00:15:03 +0530

    change the home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import web from "../src/animate.jpeg";
+import web from "../src/Home.jpg";
 import Common from './Common';
-const Home=()=>{
-    return(
+const Home = () => {
+    return (
         <>
-            <Common name='Grow your business' imgsrc={web} visit="/service" btname="Get Started"/>
+            <Common name='Grow your business' imgsrc={web} visit="/service" btname="Get Started" />
+            <p>Address:<strong>Hanuman Agencies</strong> Hosalli cross</p>
+            <p>Yadgir,Karnataka 585202</p>
+            <p>Mob.No:94483 33275</p>
+
         </>
     )
 }
